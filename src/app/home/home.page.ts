6345e3c097bf35f018b1ff018313c5dc5b076b0a
Otávio Lima <olima94@gmail.com>
2021-09-22 21:22:09 -0300

    :rocket: Implementa classificação de IMC e altera esquema de cores

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,19 +7,32 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   weight: number;
   height: number;
 
   constructor(private toastController: ToastController) {}
 
   isFormValid() {
-    return (this.height && this.weight && this.height > 0 && this.weight > 0);
+    return this.height && this.weight && this.height > 0 && this.weight > 0;
   }
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    this.showMessage(`IMC = ${imc.toFixed(2)} ${this.classificate(imc)}`);
+  }
+
+  classificate(imc: number) {
+    if (imc < 18.5) {
+      return 'MAGREZA';
+    } else if (imc >= 18.5 && imc <= 24.9) {
+      return 'NORMAL';
+    } else if (imc >= 25 && imc <= 29.9) {
+      return 'SOBREPESO';
+    } else if (imc >= 30 && imc <= 39.9) {
+      return 'OBESIDADE';
+    } else {
+      return 'OBESIDADE GRAVE';
+    }
   }
 
   async showMessage(msg: string) {
@@ -28,17 +41,15 @@ export class HomePage {
       await this.toastController.dismiss();
     }
 
-    const toast = await this.toastController.create(
-      {
-        message: msg,
-        color: 'light',
-        buttons: [
-          {
-            icon: 'close'
-          }
-        ]
-      }
-    );
+    const toast = await this.toastController.create({
+      message: msg,
+      color: 'light',
+      buttons: [
+        {
+          icon: 'close',
+        },
+      ],
+    });
     toast.present();
   }
 }
